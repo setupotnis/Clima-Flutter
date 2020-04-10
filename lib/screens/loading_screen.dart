f1d3fdb00a4c0967cf06cc19a0f0ae1d5febbdf6
Setu Potnis <setupotnis@gmail.com>
2020-04-09 21:55:54 -0400

    adding loading circle while locaiton is being fetched by API

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import '../services/networking.dart';
-import '../services/networking.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = 'a2aba233a026a47e4472ce2cdaab9409';
 
@@ -28,10 +29,25 @@ class _LoadingScreenState extends State<LoadingScreen> {
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
     var weatherData = await networkHelper.getData();
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) {
+          return LocationScreen();
+        },
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100,
+        ),
+      ),
+    );
   }
 }
