56f95e4320a79767d57e7347d97ef8978126e992
Setu Potnis <setupotnis@gmail.com>
2020-04-09 21:46:20 -0400

    able to get data for my current location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import '../services/networking.dart';
+import '../services/networking.dart';
+
+const apiKey = 'a2aba233a026a47e4472ce2cdaab9409';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,17 +11,23 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+    latitude = location.latitude;
+    longitude = location.longitude;
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+    var weatherData = await networkHelper.getData();
   }
 
   @override
--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -74,3 +74,10 @@ class _LocationScreenState extends State<LocationScreen> {
     );
   }
 }
+
+//int temperature = decodedData['main']['temp'];
+//int condition = decodedData['weather'][0]['id'];
+//String cityName = decodedData['name'];
+//print(temperature);
+//print(condition);
+//print(cityName);
